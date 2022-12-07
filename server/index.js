d399db8316d8eefd94cde428374ef219df05bd5c
Archit Purohit'git config --global user.name Archit <Architpurohit@hotmail.com>
2022-12-07 12:22:44 -0800

    adding header, centered it and started tweet text box

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ app.use(express.static("public"));
 
 // The in-memory database of tweets. It's a basic object with an array in it.
 const db = require("./lib/in-memory-db");
+const { query } = require("express");
 
 // The `data-helpers` module provides an interface to the database of tweets.
 // This simple interface layer has a big benefit: we could switch out the
@@ -31,6 +32,15 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/tweets", tweetsRoutes);
+
+app.post("/tweets", (req,res) =>{
+
+})
+
+// document,querySelector("button").addeventlistener("click", postTweet);
+// function postTweet(){
+//   alert("click registered");
+// }
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
